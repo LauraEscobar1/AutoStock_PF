eb80edddac58dde54a0cba4b4fa743a0fdf8bde3
Laura <laura.escobarca@campusucc.edu.co>
2026-06-02 18:51:58 -0500

    Add ProveedorRepository interface for Proveedor data access

---
+++ Backend/src/main/java/com/autostock/repository/ProveedorRepository.java
@@ -0,0 +1,9 @@
+package com.autostock.repository;
+
+import com.autostock.model.Proveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProveedorRepository extends JpaRepository<Proveedor, Long> {
+}
