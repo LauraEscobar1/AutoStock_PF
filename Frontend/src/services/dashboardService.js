64a65df699e8f2290c92f2ff931035a515a503ba
Laura <laura.escobarca@campusucc.edu.co>
2026-06-03 15:05:29 -0500

    changes

--- Frontend/src/services/dashboardService.js
+++
@@ -1,8 +0,0 @@
-import api from './api';
-
-export const dashboardService = {
-  contarProductos: () => api.get('/api/productos'),
-  contarMovimientos: () => api.get('/api/movimientos'),
-  contarProveedores: () => api.get('/api/proveedores'),
-  contarFacturas: () => api.get('/api/facturas')
-};
