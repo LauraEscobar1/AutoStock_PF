7c397f1adf4988eabb55b849fff87e4f831fed38
Laura <laura.escobarca@campusucc.edu.co>
2026-06-02 21:59:28 -0500

    Add TipoFactura enum and FacturaRepository interface for invoice management

---
+++ Backend/src/main/java/com/autostock/model/TipoFactura.java
@@ -0,0 +1,7 @@
+package com.autostock.model;
+
+public enum TipoFactura {
+
+    COMPRA,
+    VENTA
+}
---
+++ Backend/src/main/java/com/autostock/repository/FacturaRepository.java
@@ -0,0 +1,7 @@
+package com.autostock.repository;
+
+import com.autostock.model.Factura;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FacturaRepository extends JpaRepository<Factura, Long> {
+}
